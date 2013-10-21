f00e5450828c27200a46cdd4ccaf54eda6442bcd
Gabriel Forradellas <gabriel.forradellas@cima-it.com>
2013-10-20 23:17:27 -0500

    primera version de codigo subido a Git

---
+++ src/cima/curso/TestOne.java
@@ -0,0 +1,27 @@
+package cima.curso;
+
+/**
+ * User: gforrade
+ * Date: 2013-10-20
+ * Time: 11:15 PM
+ */
+public class TestOne {
+    private String name;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
